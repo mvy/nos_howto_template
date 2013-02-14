08eb76244ebc9a9e117ff474d1d6e495af3d906e
Yves Stadler <mvyonline@gmail.com>
2013-02-14 15:54:21 +0100

    License notice

---
+++ LICENSE.md
@@ -0,0 +1,10 @@
+NOS How-To Template 
+===================
+
+# How-To template files
+
+The template source code is licensed under the same license as Novius-OS.
+
+Licensed under [GNU Affero General Public License
+v3](http://www.gnu.org/licenses/agpl-3.0.html) or (at your option) any later
+version. 
--- views/main.view.php
+++ views/main.view.php
@@ -1,3 +1,13 @@
+<?php
+/**
+* @copyright Yves `M'vy` Stadler (2013)
+* @license GNU Affero General Public License v3 or (at your option) any later
+*   version http://www.gnu.org/licenses/agpl-3.0.html
+* @link http://os.yves-stadler.fr/
+*
+* For use with Novius-OS : https://github.com/novius-os/novius-os¬
+* http://www.novius-os.org/¬
+*/?>
 <!DOCTYPE html>
 <html> 
 <head>
